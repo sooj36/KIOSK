4ed15ba442bfb75944facb082477b0320d759ad7
choco5732 <choco5732@gmail.com>
2023-07-26 09:27:36 +0900

    [FEAT] : ⚙️ 해피스낵 클래스생성

--- app/src/main/java/com/example/kioskproject/main.kt
+++ app/src/main/java/com/example/kioskproject/main.kt
@@ -1,6 +1,7 @@
 package com.example.kioskproject
 
 fun main(){
+    /*
     println("1. 식사하실 장소를 선택해주세요.")
         println("a. 매장")
         println("b. 테이크 아웃")
@@ -8,8 +9,17 @@ fun main(){
 
         if(place == "a"){
             //매장식사
+            var pay = Payment()
+            pay.pay()
+
         }
         else if(place == "b"){
             //테이크아웃
         }
+    */
+    var pay = Payment()
+    pay.pay()
+
+    var happysnack = HappySnack()
+    happysnack.show()
 }
